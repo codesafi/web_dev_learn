e1c543c65967bc6a903c522891ebdd3907ac59cc
codesafi <mdsafiruddin24@gmail.com>
2023-09-28 21:10:41 +0530

    Show Hide login and SignUp tag

---
+++ scripts/loginSignUp.js
@@ -0,0 +1,16 @@
+function switchTab(showTab) {
+  const loginForm = document.getElementById('login_form_div');
+  const signupForm = document.getElementById('signup_form_div');
+
+  if (showTab === 'login') {
+    // Show login
+    loginForm.removeAttribute('hidden');
+    // Hide signup
+    signupForm.setAttribute('hidden', 'true');
+  } else if (showTab === 'SignUp') { // Note: Changed to 'SignUp' with a capital 'S'
+    // Hide login
+    loginForm.setAttribute('hidden', 'true');
+    // Show signup
+    signupForm.removeAttribute('hidden');
+  }
+}
